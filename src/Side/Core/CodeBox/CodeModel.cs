f7139c4a4bf6c6fbf057412c44e026f9a725350e
dimart <dimart.sp@gmail.com>
2014-05-29 02:15:58 +0400

    CodeBox: M & VM changed

--- src/Side/Core/CodeBox/CodeModel.cs
+++ src/Side/Core/CodeBox/CodeModel.cs
@@ -1,12 +1,46 @@
-﻿namespace Side.Core.CodeBox
+﻿using System;
+using System.ComponentModel;
+using Side.Interfaces;
+
+namespace Side.Core.CodeBox
 {
-    class CodeModel
+    public class CodeModel : ViewModelBase
     {
+        private string _oldCode;
+        private bool _isDirty;
+
         public ICSharpCode.AvalonEdit.Document.TextDocument Code { get; set; }
 
         public CodeModel()
         {
             Code = new ICSharpCode.AvalonEdit.Document.TextDocument();
+            Code.PropertyChanged += CodePropertyChanged;
+            Code.TextChanged += CodeOnTextChanged;
+            _oldCode = "";
+        }
+
+        public bool IsDirty
+        {
+            get { return _isDirty; }
+            set
+            {
+                _isDirty = value;
+                RaisePropertyChanged("IsDirty");
+                if (value == false)
+                {
+                    _oldCode = Code.Text;
+                }
+            }
+        }
+        
+        private void CodeOnTextChanged(object sender, EventArgs eventArgs)
+        {
+            IsDirty = (_oldCode != Code.Text);
+        }
+
+        private void CodePropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            RaisePropertyChanged("Document");
         }
     }
 }
--- src/Side/Core/CodeBox/CodeViewModel.cs
+++ src/Side/Core/CodeBox/CodeViewModel.cs
@@ -1,11 +1,65 @@
-﻿namespace Side.Core.CodeBox
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Controls;
+using Side.Interfaces;
+using Side.Interfaces.Services;
+
+namespace Side.Core.CodeBox
 {
-    public class CodeViewModel
+    public class CodeViewModel : ViewModelBase
     {
         private CodeModel _model;
-        
-        public CodeViewModel()
-        {           
+        private ILoggerService _logger;
+
+        private string _title = null;
+        private IWorkspace _workspace;
+
+        public CodeViewModel(IWorkspace workspace, ILoggerService logger)
+        {
+            _workspace = workspace;
+            _logger = logger;
         }
+
+        public CodeModel Model
+        {
+            get { return _model; }
+            set
+            {
+                if (_model != null)
+                {
+                    _model.PropertyChanged -= Model_PropertyChanged;
+                }
+                if (value == null) return;
+                _model = value;
+                _model.PropertyChanged += Model_PropertyChanged;
+            }
+        }
+
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            RaisePropertyChanged("Model");
+            RaisePropertyChanged("Title");
+        }
+
+        public UserControl View { get; set; }
+
+        public string Title
+        {
+            get
+            {
+                if (Model.IsDirty)
+                {
+                    return _title + "*";
+                }
+                return _title;
+            }
+            set
+            {
+                if (_title == value) return;
+                _title = value;
+                RaisePropertyChanged("Title");
+            }
+        }
+
     }
 }
